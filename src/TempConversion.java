94f5dd730df82c35cd0e1b0ba9a40f321d6c0c71
bryan <bmaus719@mycr.redwoods.edu>
2023-02-26 22:04:32 -0800

    Java Assignment 005 Completed

--- src/TempConversion.java
+++ src/TempConversion.java
@@ -1,3 +1,16 @@
+/**
+ *
+ * @arthur Trevor Harman
+ * @arthur Bryan Maus
+ *
+ * @since Version 1.0
+ * @date 02-26-2023
+ *
+ * This program is designed to convert from one temperature unit to another using user input
+ *
+ */
+
+
 import java.util.Scanner;
 
 public class TempConversion {
@@ -41,54 +54,63 @@ public class TempConversion {
     public static void main(String[] args) {
 
         while(true) {
-            System.out.println("Enter the Unit to convert from: \n");
+        // Prompt user for first temperature type
+            System.out.println("\nEnter the Unit to convert from: \n");
             String temp1 = getUnitChoice();
-            switch (temp1){
-                case "q": return;
-                case "Q": return;
+            switch (temp1) {
+                case "q", "Q" -> {
+                    return;
+                }
             }
+        // Prompt user for second temperature type
             System.out.println("Enter the Unit to convert to: \n");
             String temp2 = getUnitChoice();
-            switch (temp2){
-                case "q": return;
-                case "Q": return;
+            switch (temp2) {
+                case "q", "Q" -> {
+                    return;
+                }
             }
-            switch(temp1+temp2){
-                case "fc": double f2C=convertF2C(getTemp(temp1));
+            // Convert Fahrenheit to Celsius
+            switch (temp1 + temp2) {
+                case "fc" -> {
                     double tempinp = getTemp(temp1);
-                    System.out.println("Result " + tempinp + " " + f2C + " " + temp2);
-                //System.out.printf("\n %s converted to %s equals %d \n\n", temp1, temp2, f2C);
-                    break;
-                case "fk": double f2k=convertF2K(getTemp(temp1));
-                    System.out.printf("\n %s converted to %s equals %d \n\n", temp1, temp2, f2k);
-                System.out.println(f2k);
-                    break;
-                case "cf": double c2f=convertC2F(getTemp(temp1));
-                    System.out.printf("\n%s converted to %s equals %d\n\n", temp1, temp2, c2f);
-                    System.out.println(c2f);
-                    break;
-                case "ck": double c2k=convertC2K(getTemp(temp1));
-                    System.out.printf("\n%s converted to %s equals %d\n\n", temp1, temp2, c2k);
-                    System.out.println(c2k);
-                    break;
-                case "kf": double k2f=convertK2F(getTemp(temp1));
-                    System.out.printf("\n%s converted to %s equals %d\n\n", temp1, temp2, k2f);
-                    System.out.println(k2f);
-                    break;
-                case "kc": double k2c=convertK2C(getTemp(temp1));
-                    System.out.printf("\n%s converted to %s equals %d\n\n", temp1, temp2, k2c);
-                    System.out.println(k2c);
-                    break;
-                default:
-                    System.out.println(temp1 + " and " + temp2 + " do not match any temperature abbreviation combinations that I know");
-                    break;
+                    double f2C = convertF2C(tempinp);
+                    System.out.printf("\nResult %f\u00B0%s is %f\u00B0%s\n", tempinp, temp1, f2C, temp2);
+                }
+            // Convert Fahrenheit to Kelvin
+                case "fk" -> {
+                    double tempinp = getTemp(temp1);
+                    double f2k = convertF2K(tempinp);
+                    System.out.printf("\nResult %f\u00B0%s is %f\u00B0%s\n", tempinp, temp1, f2k, temp2);
+                }
+            // Convert Celsius to Fahrenheit
+                case "cf" -> {
+                    double tempinp = getTemp(temp1);
+                    double c2f = convertC2F(tempinp);
+                    System.out.printf("\nResult %f\u00B0%s is %f\u00B0%s\n", tempinp, temp1, c2f, temp2);
+                }
+            // Convert Celsius to Kelvin
+                case "ck" -> {
+                    double tempinp = getTemp(temp1);
+                    double c2k = convertC2K(tempinp);
+                    System.out.printf("\nResult %f\u00B0%s is %f\u00B0%s\n", tempinp, temp1, c2k, temp2);
+                }
+            // Convert Kelvin to Fahrenheit
+                case "kf" -> {
+                    double tempinp = getTemp(temp1);
+                    double k2f = convertK2F(tempinp);
+                    System.out.printf("\nResult %f\u00B0%s is %f\u00B0%s\n", tempinp, temp1, k2f, temp2);
+                }
+            // Convert Kelvin to Celsius
+                case "kc" -> {
+                    double tempinp = getTemp(temp1);
+                    double k2c = convertK2C(tempinp);
+                    System.out.printf("\nResult %f\u00B0%s is %f\u00B0%s\n", tempinp, temp1, k2c, temp2);
+                }
+            // Default output if input does not match temperature abbreviations
+                default ->
+                        System.out.println(temp1 + " and " + temp2 + " do not match any temperature abbreviation combinations that I know");
             }
-
-
-
-            getTemp(getUnitChoice());
-
-
         }
     }
 }
